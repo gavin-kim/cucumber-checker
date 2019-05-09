5b71ca6845c24e146ea57e3d6d040328c4f3660a
Yoonkwan Kim <yoonkwan.kim@infor.com>
2019-05-09 18:12:03 -0400

    .

--- src/main/kotlin/contoller/ApiController.kt
+++ src/main/kotlin/contoller/ApiController.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-@RequestMapping("/api")
 class ApiController {
 
     @RequestMapping("/", produces = ["application/json"])
